1215e0ee8176f2d0723db053a2ba6812d28768fb
Fernando <Fernando@Fernando-PC>
2016-10-24 00:24:40 -0300

    Melhorias no Bean de Medicamentos e HibernateFactory

--- src/br/com/sgh/managedbean/MedicamentoBean.java
+++ src/br/com/sgh/managedbean/MedicamentoBean.java
@@ -1,31 +1,58 @@
 package br.com.sgh.managedbean;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
+
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortOrder;
 
 import br.com.sgh.dao.MedicamentoDao;
 import br.com.sgh.model.Medicamento;
 
 @ManagedBean(name="medicamentoBean")
-@RequestScoped
+@ViewScoped
 public class MedicamentoBean {
 
 	private List<Medicamento> listaMedicamentos;
-	
+
+	private LazyDataModel<Medicamento> lazyDataModel;
 	@Inject
 	private MedicamentoDao medicamentoDao;
 
 	public List<Medicamento> getListaMedicamentos() {
 		if(listaMedicamentos == null){
-			listaMedicamentos = medicamentoDao.listarTodos(Medicamento.class);			
+			listaMedicamentos = medicamentoDao.listarTodos();			
 		}
 		return listaMedicamentos;
 	}
 
 	public void setListaMedicamentos(List<Medicamento> listaMedicamentos) {
 		this.listaMedicamentos = listaMedicamentos;
-	}		
+	}
+
+	public LazyDataModel<Medicamento> getLazyDataModel() {
+		if(lazyDataModel == null){
+			lazyDataModel = new LazyDataModel<Medicamento>(){
+				private static final long serialVersionUID = -5786183477810450507L;
+
+				@SuppressWarnings("rawtypes")
+				@Override
+				public List<Medicamento> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map filters) {
+					List<Medicamento> data = medicamentoDao.listarTodos(first, pageSize);
+					return data;
+				}
+				
+			};
+			lazyDataModel.setRowCount(medicamentoDao.obterQuantidadeTotal().intValue());
+		}
+		return lazyDataModel;
+	}
+
+	public void setLazyDataModel(LazyDataModel<Medicamento> lazyDataModel) {
+		this.lazyDataModel = lazyDataModel;
+	}	
 }
